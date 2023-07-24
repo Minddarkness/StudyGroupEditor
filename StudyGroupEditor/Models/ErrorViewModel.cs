eded20baa35ea3d855ce5ade6aaa65fa6148895c
Anastasiya Levina <miss.levinaayandex.ru>
2023-07-24 09:59:13 +0700

    MVC template

---
+++ StudyGroupEditor/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudyGroupEditor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
